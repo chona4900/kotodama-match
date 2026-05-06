84374f1b1d80929f4533f071735197b747c143f6
chona4900 <chona4900@example.com>
2026-05-06 16:54:44 +0900

    style: fix horizontal overflow on oyatsu list caused by transform scale

--- main.js
+++ main.js
@@ -1932,8 +1932,8 @@
                 if (w === 'だんだんよくなる未来はあかるい') displayWord = 'だんだんよくなる<br>未来はあかるい';
                 else if (w === 'このことがダイヤモンドにかわります') displayWord = 'このことがダイヤ<br>モンドにかわります';
                 else if (w === '宇宙の調和に感謝します') displayWord = '宇宙の調和に<br>感謝します';
-                else if (w === 'もっと自分を愛しますもっと自分をゆるします') displayWord = '<span style=\'font-size: 1.6rem; display: inline-block; transform: scale(0.75); transform-origin: left center; white-space: nowrap;\'>もっと自分を愛します<br>もっと自分をゆるします</span>';
-                else if (w === 'どうでもいいどっちでもいいどうせうまくいくから') displayWord = '<span style=\'font-size: 1.6rem; display: inline-block; transform: scale(0.75); transform-origin: left center; white-space: nowrap;\'>どうでもいい<br>どっちでもいい<br>どうせうまくいくから</span>';
+                else if (w === 'もっと自分を愛しますもっと自分をゆるします') displayWord = '<span style=\'font-size: 1.25rem; white-space: nowrap; -webkit-text-stroke: 0.3px;\'>もっと自分を愛します<br>もっと自分をゆるします</span>';
+                else if (w === 'どうでもいいどっちでもいいどうせうまくいくから') displayWord = '<span style=\'font-size: 1.25rem; white-space: nowrap; -webkit-text-stroke: 0.3px;\'>どうでもいい<br>どっちでもいい<br>どうせうまくいくから</span>';
                 
                 let row = document.createElement('div');
                 row.className = 'stats-row';
